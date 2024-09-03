9f2150cab215dda139296a4599f97c45d72daecb
mateusscarin <mateus.scarin@gmail.com>
2024-09-02 22:21:51 -0300

    Initial commit :tada:

---
+++ src/test/java/com/br/bodysync/BodySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bodysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
