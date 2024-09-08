671fd7fdd7e393836f9c851c644c7c3fac7bacec
mateusscarin <mateus.scarin@gmail.com>
2024-09-08 17:30:48 -0300

    feat: Objective entity, DTO and mapper

---
+++ src/main/java/com/br/bodysync/model/Objective.java
@@ -0,0 +1,104 @@
+package com.br.bodysync.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
+public class Objective implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updateDate;
+
+    public Objective() {
+    }
+
+    @PrePersist
+    private void prePersist() {
+        setStatus(true);
+        setCreatedDate(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        setUpdateDate(LocalDateTime.now());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+}
