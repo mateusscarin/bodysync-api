75f3abb98903a6c5de869d86ce4d38a60895a627
Carlos <carloscaetanofernandes@hotmail.com>
2024-09-18 21:44:45 -0300

    feat: Metric CRUD

---
+++ src/main/java/com/br/bodysync/controller/MetricController.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.br.bodysync.controller;
+
+import com.br.bodysync.model.dto.MetricDTO;
+import com.br.bodysync.service.MetricService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Carlos
+ */
+
+@RestController
+@RequestMapping(value = "/metric")
+public class MetricController {
+    
+    @Autowired
+    private MetricService metricService;
+    
+    @PostMapping
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Criado (Created)", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = MetricDTO.class))}),
+        @ApiResponse(responseCode = "401", description = "Não Autorizado (Unauthorized)"),
+        @ApiResponse(responseCode = "404", description = "Não Encontrado (Not Found)"),
+        @ApiResponse(responseCode = "500", description = "Erro interno (Internal Server Error)")
+    })
+    public ResponseEntity<Object> save(@RequestBody MetricDTO object) throws Exception {
+        return metricService.save(object);
+    }
+
+    @GetMapping
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Criado (Created)", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = MetricDTO.class))}),
+        @ApiResponse(responseCode = "401", description = "Não Autorizado (Unauthorized)"),
+        @ApiResponse(responseCode = "404", description = "Não Encontrado (Not Found)"),
+        @ApiResponse(responseCode = "500", description = "Erro interno (Internal Server Error)")
+    })
+    public ResponseEntity<Object> findAll() throws Exception {
+        return metricService.findAll();
+    }
+
+    @GetMapping("/{idObjeto}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Criado (Created)", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = MetricDTO.class))}),
+        @ApiResponse(responseCode = "401", description = "Não Autorizado (Unauthorized)"),
+        @ApiResponse(responseCode = "404", description = "Não Encontrado (Not Found)"),
+        @ApiResponse(responseCode = "500", description = "Erro interno (Internal Server Error)")
+    })
+    public ResponseEntity<Object> findById(@PathVariable(name = "idObjeto") Long idObjeto) throws Exception {
+        return metricService.findById(idObjeto);
+    }
+
+    @PutMapping("/{idObjeto}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Criado (Created)", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = MetricDTO.class))}),
+        @ApiResponse(responseCode = "401", description = "Não Autorizado (Unauthorized)"),
+        @ApiResponse(responseCode = "404", description = "Não Encontrado (Not Found)"),
+        @ApiResponse(responseCode = "500", description = "Erro interno (Internal Server Error)")
+    })
+    public ResponseEntity<Object> edit(@PathVariable Long idObjeto, @RequestBody MetricDTO object) throws Exception {
+        return metricService.edit(idObjeto, object);
+    }
+
+    @DeleteMapping("/{idObjeto}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Criado (Created)", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = MetricDTO.class))}),
+        @ApiResponse(responseCode = "401", description = "Não Autorizado (Unauthorized)"),
+        @ApiResponse(responseCode = "404", description = "Não Encontrado (Not Found)"),
+        @ApiResponse(responseCode = "500", description = "Erro interno (Internal Server Error)")
+    })
+    public ResponseEntity<Object> delete(@PathVariable Long idObjeto) throws Exception {
+        return metricService.delete(idObjeto);
+    }
+}
---
+++ src/main/java/com/br/bodysync/service/MetricService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.br.bodysync.service;
+
+import com.br.bodysync.model.dto.MetricDTO;
+import com.br.bodysync.service.util.CrudService;
+
+/**
+ *
+ * @author Carlos
+ */
+
+public interface MetricService extends CrudService<MetricDTO>{
+    
+}
